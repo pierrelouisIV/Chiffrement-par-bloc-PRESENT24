a11a62169ea33c2855aedbcc2bc6e359e9c972d1
Gwennael Cannenpase <Gwen@uvsq.fr>
2021-04-22 09:35:08 +0200

    modif du nom des variables

--- dechiffrer.c
+++ dechiffrer.c
@@ -8,11 +8,11 @@
 #define l_permute 24
 
 // variables globales :
-int t1[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};	// S(x)
-int t2[] = {0x5, 0xe, 0xf, 0x8, 0xc, 0x1, 0x2, 0xd, 0xb, 0x4, 0x6, 0x3, 0x0, 0x7, 0x9, 0xa};	// x
+int t1_dechiffrer[] = {0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xa, 0xb, 0xc, 0xd, 0xe, 0xf};	// S(x)
+int t2_dechiffrer[] = {0x5, 0xe, 0xf, 0x8, 0xc, 0x1, 0x2, 0xd, 0xb, 0x4, 0x6, 0x3, 0x0, 0x7, 0x9, 0xa};	// x
 
-int P[] = {0, 4, 8 ,12, 16, 20, 1, 5, 9, 13, 17, 21, 2, 6, 10, 14, 
- 			18, 22, 3, 7, 11, 15, 19, 23};													// P(i) -> i
+int P_dechiffrer[] = {0, 4, 8 ,12, 16, 20, 1, 5, 9, 13, 17, 21, 2, 6, 10, 14, 
+ 			18, 22, 3, 7, 11, 15, 19, 23};													// P_dechiffrer(i) -> i
 
 
 // Fonctions complémentaires :
@@ -48,14 +48,14 @@ int recherche_dichotomique_2(int valeur, int borne_inf, int borne_sup)
 	else
 	{
 		milieu = (borne_inf+borne_sup) / 2;
-		if (valeur == t1[milieu])
+		if (valeur == t1_dechiffrer[milieu])
 			return milieu;
 		else
 		{
-			if (valeur < t1[milieu])
-				return recherche_dichotomique(valeur, borne_inf, milieu-1);
+			if (valeur < t1_dechiffrer[milieu])
+				return recherche_dichotomique_2(valeur, borne_inf, milieu-1);
 			else
-				return recherche_dichotomique(valeur, milieu+1, borne_sup);
+				return recherche_dichotomique_2(valeur, milieu+1, borne_sup);
 		}
 	}
 }
@@ -81,8 +81,8 @@ int substitution_inv(int m)
 	{
 		res1 = (m & masque) >> i*4;
 		masque >>= 4;
-		res2 = t2[recherche_dichotomique(res1, D, l_boite)];
-		res3 = concat(res3, res2);
+		res2 = t2_dechiffrer[recherche_dichotomique_2(res1, D, l_boite)];
+		res3 = concat_2(res3, res2);
 	}
 	return res3;
 }
@@ -94,26 +94,26 @@ int calcul_binaire_en_hexa_2(int *tableau)
 	int res1 = 0;
 	int res2 = 0;
 	int cpt = 0;
-	int p = 3;
+	int P_dechiffrer = 3;
 	for (int i = 23; i >= -1; --i)
 	{
 		if(cpt == 4)
 		{
-			res2 = concat(res2, res1);
+			res2 = concat_2(res2, res1);
 			cpt = 0;
-			p = 3;
+			P_dechiffrer = 3;
 			res1 = 0;
 		}
 		if (tableau[i])
 			{
-				res1 += pow(2,p);
+				res1 += pow(2,P_dechiffrer);
 				cpt++;
-				p--;
+				P_dechiffrer--;
 			}
 		else
 		{
 			cpt++;
-			p--;
+			P_dechiffrer--;
 		}
 	}
 	return res2;
@@ -134,11 +134,11 @@ int permutation_inv(int n)
 	{
 		bit = (n & masque) >> i ;
 		masque <<= 1 ;
-		nouveau[P[j]] = bit;	// remplir tableau
+		nouveau[P_dechiffrer[j]] = bit;	// remplir tableau
 		j++;
 	}
 
-	resultat = calcul_binaire_en_hexa(nouveau);
+	resultat = calcul_binaire_en_hexa_2(nouveau);
 	free(nouveau);
 	return resultat;
 }
@@ -152,9 +152,9 @@ int chiffrer_2(int mot_chiffre, int clef_maitre)
 	etat = clef_plus_etat_2(etat, sous_clefs[10]);
 	for (int i = 9; i >= 0; --i)
 	{		
-		etat = permutation(etat);
-		etat = substitution(etat);
-		etat = clef_plus_etat(etat, sous_clefs[i]);	
+		etat = permutation_inv(etat);
+		etat = substitution_inv(etat);
+		etat = clef_plus_etat_2(etat, sous_clefs[i]);	
 		printf("%X et %X \n",etat, sous_clefs[i] );
 	}
 	printf("le mot clair est : %X\n", etat);
